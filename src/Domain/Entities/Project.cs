7c858f29c5c53ab92c31bb92dded1fd12c9b2c44
wopa <p.woltschkow@gmail.com>
2021-12-18 18:21:56 +0200

    feat: define domain entities

---
+++ src/Domain/Entities/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public IEnumerable<WorkItem> WorkItems { get; set; } = Enumerable.Empty<WorkItem>();
+    }
+}
---
+++ src/Domain/Entities/WorkItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class WorkItem
+    {
+        public int Id { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public Project Project { get; set; } = null!;
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Iteration { get; set; } = string.Empty; //Todo implement iterations
+
+        public string AssignedTo { get; set; } = string.Empty; //Todo implement assigning user
+
+        public DateTime? StartDate { get; set; }
+
+        public Priority Priority { get; set; }
+    }
+}

a70be25e7d69abf51370368aa9b49307e2ba9f17
wopa <p.woltschkow@gmail.com>
2021-12-18 20:13:50 +0200

    feat: add abstraction for DbContext

---
+++ src/Application/Common/IApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Common
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Project> Projects { get; }
+
+        DbSet<WorkItem> WorkItems { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}

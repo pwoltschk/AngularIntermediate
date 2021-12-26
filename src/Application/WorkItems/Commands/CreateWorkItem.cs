dd541efe53b7d263e6f42a6645647e41b41cb1ce
wopa <p.woltschkow@gmail.com>
2021-12-26 18:28:12 +0200

    feat: implement create command for work items

---
+++ src/Application/WorkItems/Commands/CreateWorkItem.cs
@@ -0,0 +1,36 @@
+﻿using Application.Common;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.WorkItems.Commands
+{
+
+    public record CreateWorkItemCommand(CreateWorkItemRequest Item) : IRequest<int>;
+
+    public class CreateWorkItemCommandHandler
+        : IRequestHandler<CreateWorkItemCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public CreateWorkItemCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateWorkItemCommand request,
+            CancellationToken cancellationToken)
+        {
+            var entity = new WorkItem
+            {
+                ProjectId = request.Item.ProjectId,
+                Title = request.Item.Title,
+            };
+
+            _context.WorkItems.Add(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return entity.Id;
+        }
+    }
+}
---
+++ src/Application/WorkItems/Requests/CreateWorkItemRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.WorkItems.Requests
+{
+    public class CreateWorkItemRequest
+    {
+        public int ProjectId { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+    }
+}

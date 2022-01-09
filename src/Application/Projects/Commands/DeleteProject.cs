c4092c7b8be5c3dc9eb52ae9d23c809fc0c02763
wopa <p.woltschkow@gmail.com>
2022-01-09 22:35:53 +0200

    feat: introduce delete command

---
+++ src/Application/Projects/Commands/DeleteProject.cs
@@ -0,0 +1,35 @@
+﻿using Application.Common;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Projects.Commands;
+
+public record DeleteProjectCommand(int Id) : IRequest;
+
+public class DeleteProjectCommandHandler
+    : AsyncRequestHandler<DeleteProjectCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteProjectCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    protected override async Task Handle(DeleteProjectCommand request,
+        CancellationToken cancellationToken)
+    {
+        var entity = await _context.Projects
+            .Where(l => l.Id == request.Id)
+            .SingleOrDefaultAsync(cancellationToken);
+
+        if (entity == null)
+        {
+            throw new Exception($"The request ID {request.Id} was not found.");
+        }
+
+        _context.Projects.Remove(entity);
+
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}

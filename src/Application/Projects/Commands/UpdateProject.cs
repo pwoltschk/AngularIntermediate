3fa90d0a273348c513258374c61cc1ee721897ce
wopa <p.woltschkow@gmail.com>
2022-01-26 19:47:01 +0200

    feat: update project command implementation

---
+++ src/Application/Projects/Commands/UpdateProject.cs
@@ -0,0 +1,32 @@
+﻿using Application.Common;
+using Application.Projects.Requests;
+using MediatR;
+
+namespace Application.Projects.Commands;
+
+public record UpdateProjectCommand(UpdateProjectRequest Project) : IRequest;
+
+public class UpdateProjectCommandHandler
+    : AsyncRequestHandler<UpdateProjectCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public UpdateProjectCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    protected override async Task Handle(UpdateProjectCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await _context.Projects.FindAsync(request.Project.Id);
+
+        if (entity == null)
+        {
+            throw new Exception($"The request ID {entity.Id} was not found.");
+        }
+
+        entity.Title = request.Project.Title;
+
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ src/Application/Projects/Requests/UpdateProjectRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Projects.Requests;
+
+public class UpdateProjectRequest
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+}

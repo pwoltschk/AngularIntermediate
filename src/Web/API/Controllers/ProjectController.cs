d57c9f09ac0176d1c0c4a066d4bfe7fdd56690fa
wopa <p.woltschkow@gmail.com>
2022-01-29 22:39:53 +0200

    feat: create project controller

---
+++ src/Web/API/Controllers/ProjectController.cs
@@ -0,0 +1,49 @@
+﻿using Application.Projects.Commands;
+using Application.Projects.Requests;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class ProjectController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProjectController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<int>> PostProject(
+            CreateProjectRequest request)
+        {
+            return await _mediator.Send(new CreateProjectCommand(request));
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> PutProject(int id,
+            UpdateProjectRequest request)
+        {
+            if (id != request.Id) return BadRequest();
+
+            await _mediator.Send(new UpdateProjectCommand(request));
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> DeleteProject(int id)
+        {
+            await _mediator.Send(new DeleteProjectCommand(id));
+
+            return NoContent();
+        }
+    }
+}

dd41f8fed19c9c55f28126720a018ae777daa7b3
wopa <p.woltschkow@gmail.com>
2022-02-25 22:36:23 +0200

    feat: configure Entity Framework Core

---
+++ src/Infrastructure/Data/Configurations/WorkItemConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configurations;
+
+public class WorkItemConfiguration : IEntityTypeConfiguration<WorkItem>
+{
+    public void Configure(EntityTypeBuilder<WorkItem> builder)
+    {
+        builder.Property(t => t.Title)
+            .HasMaxLength(150)
+            .IsRequired();
+
+        builder.Property(t => t.Description)
+            .HasMaxLength(8000);
+    }
+}

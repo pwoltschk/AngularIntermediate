06b01ad9220439bb44fa03c9d17fb43bdb18ba8a
wopa <p.woltschkow@gmail.com>
2022-02-25 11:26:23 +0200

    feat: set up database context

---
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Application.Common;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Project> Projects => Set<Project>();
+
+    public DbSet<WorkItem> WorkItems => Set<WorkItem>();
+
+}
---
+++ src/Infrastructure/Data/ApplicationDbContextInitialiser.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class ApplicationDbContextInitialiser
+{
+    private readonly ApplicationDbContext _context;
+
+    public ApplicationDbContextInitialiser(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public void Initialise()
+    {
+        if (_context.Database.IsSqlServer())
+        {
+            _context.Database.Migrate();
+        }
+        else
+        {
+            _context.Database.EnsureCreated();
+        }
+    }
+
+    public void Seed()
+    {
+        if (_context.Projects.Any())
+        {
+            return;
+        }
+
+        var list = new Project
+        {
+            Title = "Develop Kanban board",
+            WorkItems = new List<WorkItem>()
+                {
+                    new() { Title = "Implement Infrastructure Layer" },
+                    new() { Title = "Implement Frontend" },
+                }
+        };
+
+        _context.Projects.Add(list);
+        _context.SaveChanges();
+    }
+}

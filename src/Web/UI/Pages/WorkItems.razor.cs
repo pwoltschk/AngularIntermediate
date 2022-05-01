80cfaa05ae49920002eea77e78a5359e5fbb860f
wopa <p.woltschkow@gmail.com>
2022-05-01 18:06:43 +0200

    feat: implement creation of work item in UI

--- src/Web/UI/Pages/WorkItems.razor.cs
+++ src/Web/UI/Pages/WorkItems.razor.cs
@@ -6,4 +6,31 @@ public partial class WorkItems
 {
     [CascadingParameter]
     public ProjectState State { get; set; } = null!;
+
+    private WorkItemDto _newWorkItem = new();
+
+    private bool _showCreateWorkItemDialog = false;
+
+    public void ShowCreateWorkItemDialog()
+    {
+        _showCreateWorkItemDialog = true;
+    }
+
+    public void CloseCreateWorkItemDialog()
+    {
+        _showCreateWorkItemDialog = false;
+    }
+
+    public async Task AddWorkItem()
+    {
+        var itemId = await State.WorkItemClient.PostWorkItemAsync(new CreateWorkItemRequest()
+        {
+            ProjectId = State.SelectedList!.Id,
+            Title = _newWorkItem.Title
+        });
+        var workItem = new WorkItemDto { Id = itemId, Title = _newWorkItem.Title };
+        State.SelectedList!.WorkItems.Add(workItem);
+        _newWorkItem = new WorkItemDto();
+        _showCreateWorkItemDialog = false;
+    }
 }

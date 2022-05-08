5c63a27159baac4fe8ff47f47e4565fab12a2f43
wopa <p.woltschkow@gmail.com>
2022-05-08 21:12:14 +0200

    feat: include edit projects in web UI

--- src/Web/UI/Pages/Project.razor.cs
+++ src/Web/UI/Pages/Project.razor.cs
@@ -7,6 +7,9 @@ public partial class Project
     [CascadingParameter]
     public ProjectState State { get; set; } = null!;
 
+    private bool _showEditProjectDialog = false;
+    private ProjectDto _editProject = new();
+
     private bool IsSelected(ProjectDto list)
     {
         return State.SelectedList!.Id == list.Id;
@@ -18,4 +21,21 @@ public partial class Project
 
         State.SelectedList = list;
     }
+
+    private void ShowEditProjectDialog(ProjectDto project)
+    {
+        _editProject = project;
+        _showEditProjectDialog = true;
+    }
+
+    public async Task UpdateProject()
+    {
+        await State.ProjectsClient.PutProjectAsync(_editProject.Id, new UpdateProjectRequest
+        {
+            Title = _editProject.Title,
+            Id = _editProject.Id
+        });
+        _showEditProjectDialog = false;
+    }
+
 }

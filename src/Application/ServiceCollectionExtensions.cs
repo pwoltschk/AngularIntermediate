ead4ee8e48e1dab123e89972a1ee3bcac939299c
wopa <p.woltschkow@gmail.com>
2022-09-14 22:04:18 +0200

    feat: configure validation behaviour for application

--- src/Application/ServiceCollectionExtensions.cs
+++ src/Application/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using Application.Common.Behaviours;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
@@ -9,6 +11,12 @@ namespace Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+
 
             return services;
         }

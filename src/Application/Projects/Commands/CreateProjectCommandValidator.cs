752ef03ba9fc4ee8ac87e9a48c91cb343034abdd
wopa <p.woltschkow@gmail.com>
2022-10-05 22:40:38 +0200

    feat: add validators

---
+++ src/Application/Projects/Commands/CreateProjectCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using Application.Projects.Requests;
+
+namespace Application.Projects.Commands;
+public class CreateProjectCommandValidator : AbstractValidator<CreateProjectCommand>
+{
+    public CreateProjectCommandValidator()
+    {
+        RuleFor(p => p.Project).SetValidator(new CreateProjectRequestValidator());
+    }
+}
+
+
+
---
+++ src/Application/Projects/Requests/CreateProjectRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Projects.Requests;
+public class CreateProjectRequestValidator
+    : AbstractValidator<CreateProjectRequest>
+{
+    public CreateProjectRequestValidator()
+    {
+        RuleFor(v => v.Title)
+            .MaximumLength(150)
+            .NotEmpty();
+    }
+}
+

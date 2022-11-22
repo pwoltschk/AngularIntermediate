51c37e477c472c436134c557854ac83ecd08b3e4
wopa <p.woltschkow@gmail.com>
2022-11-22 21:29:08 +0200

    feat: implement logging behaviour

---
+++ src/Application/Common/Behaviours/LoggingBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common.Services;
+using MediatR.Pipeline;
+
+
+namespace YourAppNamespace.Behaviours
+{
+    public class RequestLoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest>
+        where TRequest : notnull
+    {
+        private readonly ILogger _logger;
+        private readonly IUserInfo _currentUser;
+
+        public RequestLoggingBehaviour(ILogger logger, IUserInfo currentUser)
+        {
+            _logger = logger;
+            _currentUser = currentUser;
+        }
+
+        public async Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            var requestName = typeof(TRequest).Name;
+            var userId = _currentUser.UserId ?? string.Empty;
+            var userName = $"{_currentUser.FirstName} {_currentUser.LastName}";
+
+            _logger.Information("Request: {@RequestName} | UserId: {UserId} | UserName: {UserName} | RequestData: {@Request}",
+                requestName, userId, userName, request);
+        }
+    }
+}
--- src/Application/GlobalUsings.cs
+++ src/Application/GlobalUsings.cs
@@ -3,3 +3,4 @@ global using Domain.Entities;
 global using FluentValidation;
 global using MediatR;
 global using Microsoft.EntityFrameworkCore;
+global using ILogger = Serilog.ILogger;

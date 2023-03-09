3de235770cc1a9a040c7727fa0f154a1fb67aecf
wopa <p.woltschkow@gmail.com>
2023-03-09 20:51:38 +0200

    feat: add get roles query

---
+++ src/Application/Roles/Queries/GetRoles.cs
@@ -0,0 +1,19 @@
+﻿using Application.Common.Services;
+
+namespace Application.Roles.Queries;
+public record GetRolesQuery : IRequest<IEnumerable<Role>>;
+
+public class GetRolesQueryHandler : IRequestHandler<GetRolesQuery, IEnumerable<Role>>
+{
+    private readonly IIdentityService _identityService;
+
+    public GetRolesQueryHandler(IIdentityService identityService)
+    {
+        _identityService = identityService;
+    }
+
+    public async Task<IEnumerable<Role>> Handle(GetRolesQuery request, CancellationToken cancellationToken)
+    {
+        return await _identityService.GetRolesAsync(cancellationToken);
+    }
+}

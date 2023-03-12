6bd7eb5e8dd7c2fa2c1a028a5ef747d49fc8518c
wopa <p.woltschkow@gmail.com>
2023-03-12 20:46:04 +0200

    feat: add user queries

---
+++ src/Application/Users/Queries/GetUser.cs
@@ -0,0 +1,20 @@
+﻿using Application.Common.Services;
+
+namespace Application.Users.Queries;
+public record GetUserQuery(string Id) : IRequest<User>;
+
+public class GetUserQueryHandler : IRequestHandler<GetUserQuery, User>
+{
+    private readonly IIdentityService _identityService;
+
+    public GetUserQueryHandler(IIdentityService identityService)
+    {
+        _identityService = identityService;
+    }
+
+    public async Task<User> Handle(GetUserQuery request, CancellationToken cancellationToken)
+    {
+        return await _identityService.GetUserAsync(request.Id);
+    }
+}
+
---
+++ src/Application/Users/Queries/GetUsers.cs
@@ -0,0 +1,20 @@
+﻿using Application.Common.Services;
+
+namespace Application.Users.Queries;
+
+public record GetUsersQuery() : IRequest<IEnumerable<User>>;
+
+public class GetUsersQueryHandler : IRequestHandler<GetUsersQuery, IEnumerable<User>>
+{
+    private readonly IIdentityService _identityService;
+
+    public GetUsersQueryHandler(IIdentityService identityService)
+    {
+        _identityService = identityService;
+    }
+
+    public async Task<IEnumerable<User>> Handle(GetUsersQuery request, CancellationToken cancellationToken)
+    {
+        return await _identityService.GetUsersAsync(cancellationToken);
+    }
+}

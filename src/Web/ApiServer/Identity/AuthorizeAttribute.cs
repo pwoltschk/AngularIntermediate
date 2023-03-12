3800f2855c81ae0a5bb43bb0cb77450c1cf15da0
wopa <p.woltschkow@gmail.com>
2023-03-12 21:20:56 +0200

    feat: add custom authorize attribute

---
+++ src/Web/ApiServer/Identity/AuthorizeAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiServer.Identity;
+
+public class AuthorizeAttribute : Microsoft.AspNetCore.Authorization.AuthorizeAttribute
+{
+    public AuthorizeAttribute(params string[] permissions)
+    {
+        Policy = permissions?.Any() == true ? string.Join("|", permissions) : string.Empty;
+    }
+}

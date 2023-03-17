417cd47559155f904b1a006297f0c9460bedbff6
wopa <p.woltschkow@gmail.com>
2023-03-17 19:35:36 +0200

    feat: add authorization handling

---
+++ src/Web/ApiServer/Identity/CustomAuthorizationHandler.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common.Services;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ApiServer.Identity
+{
+
+    public class CustomAuthorizationHandler : AuthorizationHandler<CustomAuthorizationRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, CustomAuthorizationRequirement requirement)
+        {
+            var permissionClaim = context.User.FindFirst(c => c.Type == nameof(Permission));
+
+            if (permissionClaim == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            var userPermission = permissionClaim.Value;
+
+            if (userPermission == requirement.Permission)
+            {
+                context.Succeed(requirement);   
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+
+}
---
+++ src/Web/ApiServer/Identity/CustomAuthorizationPolicyProvider.cs
@@ -0,0 +1,35 @@
+﻿using Application.Common.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+
+namespace ApiServer.Identity
+{
+
+    public class CustomAuthorizationPolicyProvider : DefaultAuthorizationPolicyProvider
+    {
+        private readonly AuthorizationOptions _options;
+        public CustomAuthorizationPolicyProvider(IOptions<AuthorizationOptions> options)
+            : base(options)
+        {
+            _options = options.Value;
+        }
+
+        public override async Task<AuthorizationPolicy?> GetPolicyAsync(string policyName)
+        {
+            var policy = await base.GetPolicyAsync(policyName);
+
+            if (policy == null && Permission.AllPermissions.Contains(policyName))
+            {
+                policy = new AuthorizationPolicyBuilder()
+                    .AddRequirements(new CustomAuthorizationRequirement(policyName))
+                .Build();
+
+                _options.AddPolicy(policyName, policy);
+            }
+
+            return policy;
+        }
+    }
+
+
+}

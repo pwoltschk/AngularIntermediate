938de1d60a883b1155141bf963a0726f352c5d5b
wopa <p.woltschkow@gmail.com>
2023-05-03 18:39:34 +0200

    feat: add role manager page

---
+++ src/Web/UI/Pages/RoleManager.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace UI.Pages
+{
+    public partial class RoleManager
+    {
+        [Inject]
+        public IRolesClient RolesClient { get; set; } = null!;
+
+        public RolesViewModel? Model { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadRoles();
+        }
+
+        private async Task LoadRoles()
+        {
+            Model = await RolesClient.GetRolesAsync();
+        }
+    }
+}

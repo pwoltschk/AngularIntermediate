7d9fdb7e3df23a00c0fbf7442fc9cedff099c889
wopa <p.woltschkow@gmail.com>
2023-05-18 19:26:58 +0200

    feat: initialize user manager ui

---
+++ src/Web/UI/Pages/UserManager.razor.cs
@@ -0,0 +1,17 @@
+﻿
+namespace UI.Pages
+{
+    public partial class UserManager
+    {
+        public UsersViewModel Model { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadUsers();
+        }
+
+        private async Task LoadUsers()
+        {
+        }
+    }
+}

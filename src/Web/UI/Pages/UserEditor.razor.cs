706f629dec776b92f39c28d2a48960f895bf73db
wopa <p.woltschkow@gmail.com>
2023-05-21 22:41:25 +0200

    feat: add edit user page

---
+++ src/Web/UI/Pages/UserEditor.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace UI.Pages
+{
+    public partial class UserEditor
+    {
+        [Parameter]
+        public string UserId { get; set; } = null!;
+
+        [Inject]
+        public UsersClient UsersClient { get; set; } = null!;
+
+        public UserDetailsViewModel? Model { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            Model = await UsersClient.GetUserAsync(UserId);
+        }
+    }
+}

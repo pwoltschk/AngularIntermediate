0af296ec3d4825c637417719dd38f697a17334b4
wopa <p.woltschkow@gmail.com>
2023-06-03 21:57:05 +0200

    feat: improve Project command Validator

--- src/Application/Projects/Commands/CreateProjectCommandValidator.cs
+++ src/Application/Projects/Commands/CreateProjectCommandValidator.cs
@@ -1,10 +1,28 @@
 ﻿using Application.Projects.Requests;
 
+
 namespace Application.Projects.Commands;
 public class CreateProjectCommandValidator : AbstractValidator<CreateProjectCommand>
 {
-    public CreateProjectCommandValidator()
+    private readonly IApplicationDbContext _dbContext;
+    private const string UniqueTitleErrorMessage = "The project title must be unique.";
+    private const string UniqueTitleErrorCode = "ERR_UNIQUE_TITLE";
+
+    public CreateProjectCommandValidator(IApplicationDbContext dbContext)
     {
-        RuleFor(p => p.Project).SetValidator(new CreateProjectRequestValidator());
+        _dbContext = dbContext;
+
+        RuleFor(command => command.Project)
+            .SetValidator(new CreateProjectRequestValidator());
+
+        RuleFor(command => command.Project.Title)
+            .MustAsync(IsTitleUnique)
+            .WithMessage(UniqueTitleErrorMessage)
+            .WithErrorCode(UniqueTitleErrorCode);
     }
+
+    private Task<bool> IsTitleUnique(string title, CancellationToken cancellationToken) => _dbContext.Projects
+            .AllAsync(project => project.Title != title, cancellationToken);
+
 }
+

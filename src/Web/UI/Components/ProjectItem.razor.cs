97dbfe7fbfef0758b205c930875709ad4ec8df51
wopa <p.woltschkow@gmail.com>
2023-07-07 18:34:59 +0200

    feat: implement ProjectList and ProjectItem Component

---
+++ src/Web/UI/Components/ProjectItem.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace UI.Components;
+public partial class ProjectItem
+{
+    [Parameter] 
+    public ProjectDto Project { get; set; } = new();
+
+    [Parameter] 
+    public bool IsSelected { get; set; }
+
+    [Parameter] 
+    public EventCallback OnSelect { get; set; }
+
+    [Parameter] 
+    public EventCallback OnEdit { get; set; }
+
+    [Parameter] 
+    public EventCallback OnDelete { get; set; }
+}
---
+++ src/Web/UI/Components/ProjectList.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace UI.Components;
+public partial class ProjectList
+{
+    [Parameter] 
+    public List<ProjectDto> Projects { get; set; } = new();
+
+    [Parameter] 
+    public ProjectDto? SelectedProject { get; set; }
+
+    [Parameter] 
+    public EventCallback<ProjectDto> OnSelectProject { get; set; }
+
+    [Parameter] 
+    public EventCallback OnCreateProject { get; set; }
+
+    [Parameter] 
+    public EventCallback<ProjectDto> OnEditProject { get; set; }
+
+    [Parameter] 
+    public EventCallback<ProjectDto> OnDeleteProject { get; set; }
+
+    private bool IsSelected(ProjectDto project)
+    {
+        return SelectedProject?.Id == project.Id;
+    }
+}

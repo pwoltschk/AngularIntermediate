c8ff305c6879451a37afc3d6145bc1e50bd02596
wopa <p.woltschkow@gmail.com>
2023-07-14 21:03:56 +0200

    feat: implement AuthWrapper

---
+++ src/Web/UI/Identity/AuthorizeWrapper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Shared.Identity;
+namespace UI.Identity;
+
+public class AuthorizeWrapper : Microsoft.AspNetCore.Components.Authorization.AuthorizeView
+{
+    private IEnumerable<string>? _permissions;
+
+    [Parameter]
+    public IEnumerable<string> Permissions
+    {
+        get => _permissions;
+        set
+        {
+            _permissions = value;
+            Policy = _permissions != null && _permissions.Any()
+                ? string.Join("|", _permissions.Select(p => p.ToString()))
+                : string.Empty;
+        }
+    }
+}
+

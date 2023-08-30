0d25426fda21a112b124c67a4ff0a15c7ec18b7f
wopa <p.woltschkow@gmail.com>
2023-08-30 18:45:58 +0200

    fix: user assignement

--- src/Web/UI/Components/WorkItemDialog.razor.cs
+++ src/Web/UI/Components/WorkItemDialog.razor.cs
@@ -1,28 +1,46 @@
 ﻿using Microsoft.AspNetCore.Components;
 
-namespace UI.Components;
-public partial class WorkItemDialog
+namespace UI.Components
 {
-    [Parameter]
-    public List<UserDto> Users { get; set; } = new List<UserDto>(); 
-
-    [Parameter]
-    public string Title { get; set; } = "";
-
-    [Parameter]
-    public WorkItemDto WorkItem { get; set; } = new();
-
-    [Parameter]
-    public EventCallback<WorkItemDto> OnSave { get; set; }
-
-    public bool IsVisible { get; private set; }
-
-    public void Show() => IsVisible = true;
-    public void Close() => IsVisible = false;
-
-    private async Task SaveWorkItem()
+    public partial class WorkItemDialog
     {
-        await OnSave.InvokeAsync(WorkItem);
-        Close();
+        [Parameter]
+        public List<UserDto> Users { get; set; } = new List<UserDto>();
+
+        [Parameter]
+        public string Title { get; set; } = "";
+
+        [Parameter]
+        public WorkItemDto WorkItem { get; set; } = new();
+
+        [Parameter]
+        public EventCallback<WorkItemDto> OnSave { get; set; }
+
+        public bool IsVisible { get; private set; }
+
+        private string? SelectedUserId { get; set; }
+
+        public void Show()
+        {
+            IsVisible = true;
+
+            SelectedUserId = Users.FirstOrDefault(u => u.Name == WorkItem.AssignedTo)?.Id;
+        }
+
+        public void Close() => IsVisible = false;
+
+        private void UpdateAssignedTo()
+        {
+            var selectedUser = Users.FirstOrDefault(u => u.Id == SelectedUserId);
+            WorkItem.AssignedTo = selectedUser?.Name;
+        }
+
+        private async Task SaveWorkItem()
+        {
+            UpdateAssignedTo();
+
+            await OnSave.InvokeAsync(WorkItem);
+            Close();
+        }
     }
 }

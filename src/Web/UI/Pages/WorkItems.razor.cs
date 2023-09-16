b5f44ccb283f46d0245e79f5d2e2901d8cf7b0b9
wopa <p.woltschkow@gmail.com>
2023-09-16 20:22:58 +0200

    chore: scaffold WorkItems page

---
+++ src/Web/UI/Pages/WorkItems.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace UI.Pages
+{
+    public partial class WorkItems
+    {
+        [Inject]
+        public IWorkItemClient WorkItemClient { get; set; } = null!;
+
+        public WorkItemsViewModel? Model { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadWorkItems();
+        }
+
+        private async Task LoadWorkItems()
+        {
+            Model = await WorkItemClient.GetWorkItemsAsync();
+        }
+
+        private string GetStageName(int stage) => stage switch
+        {
+            0 => "Planned",
+            1 => "In Progress",
+            2 => "Completed",
+            _ => "Unknown"
+        };
+    }
+}

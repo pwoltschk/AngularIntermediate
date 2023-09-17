9b7b267eb86fe991b8c7c883e749fed8a790c54b
wopa <p.woltschkow@gmail.com>
2023-09-17 18:57:05 +0200

    feat: add create and edit dialogs for WorkItems Page

--- src/Web/UI/Pages/WorkItems.razor.cs
+++ src/Web/UI/Pages/WorkItems.razor.cs
@@ -1,17 +1,31 @@
 ﻿using Microsoft.AspNetCore.Components;
 
+using UI.Components;
+
 namespace UI.Pages
 {
     public partial class WorkItems
     {
         [Inject]
         public IWorkItemClient WorkItemClient { get; set; } = null!;
+        [Inject]
+        public IUsersClient UsersClient { get; set; } = null!;
+        [Inject]
+        public IProjectClient ProjectsClient { get; set; } = null!;
 
         public WorkItemsViewModel? Model { get; set; }
+        private List<UserDto> _users = new();
+        private List<ProjectDto> _projects = new();
+        private WorkItemDto _newWorkItem = new();
+        private WorkItemDto _editWorkItem = new();
+
+        private WorkItemDialog _createWorkItemDialog = null!;
+        private WorkItemDialog _editWorkItemDialog = null!;
 
         protected override async Task OnInitializedAsync()
         {
             await LoadWorkItems();
+            await LoadUsersAndProjects();
         }
 
         private async Task LoadWorkItems()
@@ -19,12 +33,22 @@ namespace UI.Pages
             Model = await WorkItemClient.GetWorkItemsAsync();
         }
 
-        private string GetStageName(int stage) => stage switch
+        private async Task LoadUsersAndProjects()
         {
-            0 => "Planned",
-            1 => "In Progress",
-            2 => "Completed",
-            _ => "Unknown"
-        };
+            _users = (await UsersClient.GetUsersAsync()).Users.ToList();
+            _projects = (await ProjectsClient.GetProjectsAsync()).Projects.ToList();
+        }
+
+        public void ShowCreateWorkItemDialog()
+        {
+            _newWorkItem = new WorkItemDto();
+            _createWorkItemDialog.Show();
+        }
+
+        public void ShowEditWorkItemDialog(WorkItemDto workItem)
+        {
+            _editWorkItem = workItem;
+            _editWorkItemDialog.Show();
+        }
     }
 }

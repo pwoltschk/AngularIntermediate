2aa920ca8126931dd1291db26e9f38d5dad0d20b
wopa <p.woltschkow@gmail.com>
2023-09-20 18:30:46 +0200

    feat: add DomainEvents to aggregate root

--- src/Domain/Primitives/AggregateRoot.cs
+++ src/Domain/Primitives/AggregateRoot.cs
@@ -1,13 +1,26 @@
-﻿namespace Domain.Primitives
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Primitives
 {
     public abstract class AggregateRoot : AuditableEntity
     {
+        private readonly List<IDomainEvent> _domainEvents = new();
         protected AggregateRoot(int id) : base(id)
         {
         }
         protected AggregateRoot()
         {
 
+        }
+
+        [NotMapped]
+        public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+        public void ClearEvents() => _domainEvents.Clear();
+
+        public void AddEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
         }
     }
 }
